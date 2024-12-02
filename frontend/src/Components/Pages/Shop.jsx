9a68f6506c6c81f3b9e2f522c4c856eea5357d2d
me-sajib <me.mrsajib@gmail.com>
2024-12-02 19:41:04 +0600

    add frontend & backend dir

---
+++ frontend/src/Components/Pages/Shop.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from '../Hero/Hero'
+import Popular from '../Popular/Popular'
+import Offers from '../Offers/Offers'
+import NewsLetter from '../NewsLetter/NewsLetter'
+
+import NewCollections from '../NewCollection/NewCollections'
+
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
+      
+    </div>
+  )
+}
+
+export default Shop
